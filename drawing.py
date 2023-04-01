606a164171fdcfbfaad3e3e763a0ede1150de41b
Олег <bonbarcovoleg55@gmail.com>
2023-04-01 12:00:54 +0300

    update add mini map

--- drawing.py
+++ drawing.py
@@ -56,13 +56,16 @@ class Drawing:
         display_fps = str(int(clock.get_fps()))
         render = self.font.render(display_fps, 0, DARKORANGE)
         self.sc.blit(render, FPS_POS)
-        
-    def mini_map (self, player):
+
+    def mini_map(self, player):
         self.sc_map.fill(BLACK)
         map_x, map_y = player.x // MAP_SCALE, player.y // MAP_SCALE
-        pygame.draw.circle(self.sc_map, RED, (int(map_x), int(map_y)), 5)
         pygame.draw.line(self.sc_map, YELLOW, (map_x, map_y), (map_x + 12 * math.cos(player.angle),
-                                                               map_y + 12 * math.sin(player.angle)), 2)
+                                                 map_y + 12 * math.sin(player.angle)), 2)
+        pygame.draw.circle(self.sc_map, RED, (int(map_x), int(map_y)), 5)
+        for x, y in mini_map:
+            pygame.draw.rect(self.sc_map, DARKBROWN, (x, y, MAP_TILE, MAP_TILE))
+        self.sc.blit(self.sc_map, MAP_POS)
 
         for x, y in mini_map:
             pygame.draw.rect(self.sc_map, GREEN, (x, y, MAP_TILE, MAP_TILE))
